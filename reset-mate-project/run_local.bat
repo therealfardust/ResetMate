1d53c0281e006ee45fe91a9add3cdd0a8697c2a5
therealfardust <hqpy.fardust@gmail.com>
2026-09-22 22:46:52 -0400

    Delete reset-mate-project directory

--- reset-mate-project/run_local.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0web"
-echo Reset Mate Lab: http://localhost:8123
-python -m http.server 8123
--- reset-mate-project/run_local.ps1
+++
@@ -1,3 +0,0 @@
-Set-Location "$PSScriptRoot\web"
-Write-Host "Reset Mate Lab: http://localhost:8123"
-python -m http.server 8123
